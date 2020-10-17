ecd240c8e4ef469d1df40d803f21af434418a358
seyhaphan <phan.seiha2017@gmail.com>
2020-10-17 23:58:04 +0700

    testing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Deploy ReactJS from github</h1>
+      <h1>Deploy ReactJS from github 2</h1>
     </div>
   );
 }
